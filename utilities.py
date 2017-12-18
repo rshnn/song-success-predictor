68b937d6249ab20ec759c4218dd4dbd22ce5b7f3
rshn-dramamine <roshanpatel06@gmail.com>
2017-12-18 02:09:37 -0500

    adds svr implementation

--- utilities.py
+++ utilities.py
@@ -288,29 +288,47 @@ class Utilities(object):
         y_true_training = training_DF['song_hotttnesss']
         y_true_testing = testing_DF['song_hotttnesss']
 
-        linreg = SVR()
-        linreg.fit(X, y_true_training)
+        # linreg = SVR(kernel='poly', C=1e3, degree=2)
+        # linreg.fit(X, y_true_training)
+
+        # svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+        # svr_rbf.fit(X, y_true_training)
+        svr_lin = SVR(kernel='linear', C=0.5)
+        svr_lin.fit(X, y_true_training)
+        # svr_poly = SVR(kernel='poly', C=1e3, degree=2)
+        # svr_poly.fit(X, y_true_training)
+        # y_rbf = svr_rbf.fit(X, y).predict(X)
+        # y_lin = svr_lin.fit(X, y).predict(X)
+        # y_poly = svr_poly.fit(X, y).predict(X)
+
 
         # Predicting the training_set
-        y_pred_training = linreg.predict(X)
+        # y_pred_training_rbf = svr_rbf.predict(X)
+        y_pred_training_lin = svr_lin.predict(X)
+        # y_pred_training_poly = svr_poly.predict(X)
 
         # Predicting the testing_set
-        y_pred_testing = linreg.predict(testing_DF[X_cols])
+        # y_pred_testing_rbf = svr_rbf.predict(testing_DF[X_cols])
+        y_pred_testing_lin = svr_lin.predict(testing_DF[X_cols])
+        # y_pred_testing_poly = svr_poly.predict(testing_DF[X_cols])
 
 
         # Calculating errors
-        mean_abs = metrics.mean_absolute_error(y_true_training, y_pred_training) 
-        mean_sq =  metrics.mean_squared_error(y_true_training, y_pred_training)
-        mean_err = np.sqrt(metrics.mean_squared_error(y_true_training, y_pred_training))
-        training_error = (mean_abs, mean_sq, mean_err)
+        # mean_abs_rbf = metrics.mean_squared_error(y_true_training, y_pred_training_rbf) 
+        mean_abs_lin = metrics.mean_squared_error(y_true_training, y_pred_training_lin) 
+        # mean_abs_poly = metrics.mean_squared_error(y_true_training, y_pred_training_poly) 
+        training_error = (0, mean_abs_lin, 0)
+        # training_error = (mean_abs_rbf, mean_abs_lin, mean_abs_poly)
+        
 
-        mean_abs = metrics.mean_absolute_error(y_true_testing, y_pred_testing) 
-        mean_sq =  metrics.mean_squared_error(y_true_testing, y_pred_testing)
-        mean_err = np.sqrt(metrics.mean_squared_error(y_true_testing, y_pred_testing))
-        testing_error = (mean_abs, mean_sq, mean_err)
+        # mean_abs_rbf = metrics.mean_squared_error(y_true_testing, y_pred_testing_rbf) 
+        mean_abs_lin = metrics.mean_squared_error(y_true_testing, y_pred_testing_lin) 
+        # mean_abs_poly = metrics.mean_squared_error(y_true_testing, y_pred_testing_poly)
+        testing_error = (0, mean_abs_lin, 0)
+        # testing_error = (mean_abs_rbf, mean_abs_lin, mean_abs_poly)
 
         # Getting std deviation of hotttnesss    
         total_DF = training_DF.append(testing_DF)
         hot_std = total_DF['song_hotttnesss'].std()
 
-        return linreg, training_error, testing_error, hot_std
+        return training_error, testing_error, hot_std
